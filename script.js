e11c46fb1a0e845686a3de70a70c55bffe399aba
Jsummers1 <julie.summers2000@gmail.com>
2021-09-24 15:47:01 -0500

    fixed moment issue

--- script.js
+++ script.js
@@ -1,25 +1,25 @@
 // display current time and date
-var todaysDate = moment().format("MMMM Do YYYY, h:mm:ss a");
-$("#currentDay").html(todaysDate);
+var todayDate = moment().format("MMMM Do YYYY, h:mm:ss a");
+$("#currentDay").html(todayDate);
 
 
 $(document).ready(function () {
   // saveBtn click listener
   $(".saveBtn").on("click", function () {
-    // Get valueof the task description in JQuery
+    // Get value of the task description
     var text = $(this).siblings(".task").val();
     var time = $(this).parent().attr("id");
 
     // Save text in local storage
     localStorage.setItem(time, text);
-  });
+  })
 
   function timeTracker() {
     //get current time - hours
     var timeNow = moment().hour();
 
     // loop over time slots
-    $(".time-slot").each(function () {
+    $(".time-block").each(function () {
       var blockTime = parseInt($(this).attr("id").split("hour")[1]);
 
       // To check the time and add the classes for background indicators
@@ -41,15 +41,15 @@ $(document).ready(function () {
 
   // Get item from local storage if any
   $("#hourOne.task").val(localStorage.getItem("hourOne"));
-  $("#hourTwo .task").val(localStorage.getItem("hourTwo"));
+  $("#hourTwo.task").val(localStorage.getItem("hourTwo"));
   $("#hourThree.task").val(localStorage.getItem("hourThree"));
-  $("#hourFour .task").val(localStorage.getItem("hourFour"));
-  $("#hourFive .task").val(localStorage.getItem("hourFive"));
-  $("#hourSix .task").val(localStorage.getItem("hourSix"));
-  $("#hourSeven .task").val(localStorage.getItem("hourSeven"));
-  $("#hourEight .task").val(localStorage.getItem("hourEight"));
-  $("#hourNine .task").val(localStorage.getItem("hourNine"));
-  $("#hourTen .task").val(localStorage.getItem("hourTen"));
+  $("#hourFour.task").val(localStorage.getItem("hourFour"));
+  $("#hourFive.task").val(localStorage.getItem("hourFive"));
+  $("#hourSix.task").val(localStorage.getItem("hourSix"));
+  $("#hourSeven.task").val(localStorage.getItem("hourSeven"));
+  $("#hourEight.task").val(localStorage.getItem("hourEight"));
+  $("#hourNine.task").val(localStorage.getItem("hourNine"));
+  $("#hourTen.task").val(localStorage.getItem("hourTen"));
 
   timeTracker();
 });
